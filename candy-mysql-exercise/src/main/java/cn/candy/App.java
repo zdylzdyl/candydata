10383cf873ead6995d14b82a8a7e08eb4a166855
zdylzdyl <zdylzdyl@qq.com>
2020-01-16 08:30:17 +0800

    all

---
+++ candy-mysql-exercise/src/main/java/cn/candy/App.java
@@ -0,0 +1,13 @@
+package cn.candy;
+
+/**
+ * Hello world!
+ *
+ * @author buxi
+ * @date 2020-01-15
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
